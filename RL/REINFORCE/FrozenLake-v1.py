5e4d242952e54206e6b70f6d2b04853a5eed148c
erich313 <songzhi313@gmail.com>
2025-06-18 00:32:45 +0800

    minor Update on REINFORCE algorithm
    minor Update on REINFORCE algorithm

--- RL/REINFORCE/FrozenLake-v1.py
+++ RL/REINFORCE/FrozenLake-v1.py
@@ -17,9 +17,9 @@ class Agent:
         self.gamma = 0.98
 
 
-    def run(self, training, render, episodes, slippery):
+    def run(self, render, episodes, slippery):
         # Create environment
-        original_env = gym.make('FrozenLake-v1', map_name="4x4", render_mode=render, is_slippery=None, max_episode_steps=200)
+        original_env = gym.make('FrozenLake-v1', map_name="8x8", render_mode=render, is_slippery=None, max_episode_steps=200)
         env = CustomEnvWrapper(original_env, slippery=slippery)
 
         num_states = env.observation_space.n  # observation space, for 4x4 map, 16 states(0-15), for 8x8 map, 64 states(0-63)
@@ -68,16 +68,51 @@ class Agent:
 
         # Close environment
         env.close()
+        torch.save(reinforce.policy_net.state_dict(), "frozen_lake_reinforce.pt")
+        dp.graphing()
 
-        if training:
-            # Save policy
-            # torch.save(policy_dqn.state_dict(), "frozen_lake_dql.pt")
+    def test(self, render, episodes, slippery):
+        # Create environment
+        original_env = gym.make('FrozenLake-v1', map_name="4x4", render_mode=render, is_slippery=None, max_episode_steps=100)
+        env = CustomEnvWrapper(original_env, slippery=slippery)
 
-            # Generate a graph
-            dp.graphing()
+        num_states = env.observation_space.n  # observation space, for 4x4 map, 16 states(0-15), for 8x8 map, 64 states(0-63)
+        num_actions = env.action_space.n  # action space, 4 actions: 0=left,1=down,2=right,3=up
+
+        policynet = PolicyNet(num_states, num_actions, 128)
+        # Load the trained model
+        policynet.load_state_dict(torch.load("frozen_lake_reinforce.pt"))
+
+        # Switch the model to evaluation mode
+        policynet.eval()
+
+        for episode in range(episodes):
+            state = env.reset()[0]  # Initialize to state 0
+            state = F.one_hot(torch.tensor(state), num_classes=num_states).float().unsqueeze(dim=0)
+
+            terminated = False      # True when agent falls in hole or reached goal
+            truncated = False       # True when agent takes more than 100 actions    
+
+            total_reward = 0
+            for t in itertools.count():
+                with torch.no_grad():
+                    action = policynet(state).squeeze().argmax()
+
+                # The agent take a step
+                new_state,reward,terminated,truncated,_ = env.step(action.item())
+                new_state = F.one_hot(torch.tensor(new_state), num_classes=num_states).float().unsqueeze(dim=0)
+                total_reward += reward
+
+                if terminated or truncated:
+                    break
+
+                state = new_state
+
+        # Close environment
+        env.close()
 
 
 if __name__ == "__main__":
     agent = Agent()
-    agent.run(training=True, render=None, episodes=2500, slippery=False)
-    # agent.run(training=False, render="human", episodes=5, slippery=True)
+    agent.run(render=None, episodes=2500, slippery=None)
+    # agent.test(render="human", episodes=5, slippery=True)
--- RL/REINFORCE/dp.py
+++ RL/REINFORCE/dp.py
@@ -25,13 +25,10 @@ class DataProcessor:
         sum_rewards = np.zeros(self.episodes)
         for x in range(self.episodes):
             sum_rewards[x] = np.sum(self.rewards[max(0, x-100):(x+1)])
-        plt.subplot(121) # plot on a 1 row x 2 col grid, at cell 1
         plt.plot(sum_rewards)
-        
-        # Plot epsilon decay (Y-axis) vs episodes (X-axis)
-        plt.subplot(122) # plot on a 1 row x 2 col grid, at cell 2
-        plt.plot(self.epsilons)
-        
+                
         # Save plots
-        plt.savefig('frozen_lake_dql.png')
+        plt.savefig('frozen_lake_reinforce.png')
+
+        plt.close()
         
