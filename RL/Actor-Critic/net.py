69b66a3c7135b33d48605471a3f2929b00475fb3
SUNG-CHIH HUANG <songzhi313@gmail.com>
2025-07-04 03:35:34 +0800

    Actor-Critic Update
    Actor-Critic Method used, with batch sampling

---
+++ RL/Actor-Critic/net.py
@@ -0,0 +1,25 @@
+from torch import nn
+import torch.nn.functional as F
+
+
+class PolicyNet(nn.Module):
+    def __init__(self, state_dim, action_dim, hidden_dim):
+        super().__init__()
+        self.fc1 = nn.Linear(state_dim, hidden_dim)
+        self.fc2 = nn.Linear(hidden_dim, action_dim)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        return F.softmax(self.fc2(x), dim=1)
+
+
+class ValueNet(nn.Module):
+    def __init__(self, state_dim, hidden_dim):
+        super().__init__()
+        self.fc1 = nn.Linear(state_dim, hidden_dim)
+        self.fc2 = nn.Linear(hidden_dim, 1)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        return self.fc2(x)
+    
--- RL/Actor-Critic/test.py
+++ RL/Actor-Critic/test.py
@@ -1,1 +1,143 @@
-print('test2')
+
+
+import gymnasium as gym
+import torch
+import torch.nn.functional as F
+import itertools
+
+from algo import ActorCritic
+from custom_env import CustomEnvWrapper
+from dp import DataProcessor
+
+
+class Agent:
+    def __init__(self):
+        # Q-learning parameters
+        self.gamma = 0.99
+        self.lr_actor = 0.001
+        self.lr_critic = 0.001
+        # self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.device = torch.device("cpu")
+
+
+    def run(self, render, episodes, slippery):
+        # Create environment
+        original_env = gym.make('FrozenLake-v1', map_name="8x8", render_mode=render, is_slippery=None, max_episode_steps=200)
+        env = CustomEnvWrapper(original_env, slippery=slippery)
+
+        num_states = env.observation_space.n  # observation space, for 4x4 map, 16 states(0-15), for 8x8 map, 64 states(0-63)
+        num_actions = env.action_space.n  # action space, 4 actions: 0=left,1=down,2=right,3=up
+
+        # Initialize DataProcessor
+        dp = DataProcessor(episodes)
+
+        actorcritic = ActorCritic(num_states, 128, num_actions, self.lr_actor, self.lr_critic, self.gamma, self.device)
+            
+        for episode in range(episodes):
+            state = env.reset()[0] 
+
+            terminated = False      # True when agent falls in hole or reached goal
+            truncated = False       # True when agent takes more than 100 actions    
+
+            total_reward = 0
+            for t in itertools.count():
+                transition = []
+
+                action = actorcritic.take_action(state, num_states)
+
+                # The agent take a step
+                next_state,reward,terminated,truncated,_ = env.step(action)
+
+                transition.append(state)
+                transition.append(action)
+                transition.append(reward)
+                transition.append(next_state)
+                transition.append(True if terminated or truncated else False)
+
+                actorcritic.update(transition, num_states)
+
+                total_reward += reward
+
+                if terminated or truncated:
+                    break
+
+                # Move to the next state
+                state = next_state
+
+            # Track rewards for graphing
+            dp.track_rewards(episode, total_reward)
+
+        # Close environment
+        env.close()
+        torch.save(actorcritic.actor.state_dict(), "frozen_lake_actor-critic.pt")
+        dp.graphing()
+
+    # def test(self, render, episodes, slippery):
+    #     # Create environment
+    #     original_env = gym.make('FrozenLake-v1', map_name="8x8", render_mode=render, is_slippery=None, max_episode_steps=200)
+    #     env = CustomEnvWrapper(original_env, slippery=slippery)
+
+    #     num_states = env.observation_space.n  # observation space, for 4x4 map, 16 states(0-15), for 8x8 map, 64 states(0-63)
+    #     num_actions = env.action_space.n  # action space, 4 actions: 0=left,1=down,2=right,3=up
+
+    #     policynet = PolicyNet(num_states, num_actions, 128)
+    #     # Load the trained model
+    #     policynet.load_state_dict(torch.load("frozen_lake_actor-critic.pt"))
+
+    #     # Switch the model to evaluation mode
+    #     policynet.eval()
+
+    #     for episode in range(episodes):
+    #         state = env.reset()[0]  # Initialize to state 0
+    #         state = F.one_hot(torch.tensor(state), num_classes=num_states).float().unsqueeze(dim=0)
+
+    #         terminated = False      # True when agent falls in hole or reached goal
+    #         truncated = False       # True when agent takes more than 100 actions    
+
+    #         total_reward = 0
+    #         for t in itertools.count():
+    #             with torch.no_grad():
+    #                 action = policynet(state).squeeze().argmax()
+
+    #             # The agent take a step
+    #             new_state,reward,terminated,truncated,_ = env.step(action.item())
+    #             new_state = F.one_hot(torch.tensor(new_state), num_classes=num_states).float().unsqueeze(dim=0)
+    #             total_reward += reward
+
+    #             if terminated or truncated:
+    #                 break
+
+    #             state = new_state
+
+    #     # Close environment
+    #     env.close()
+
+
+if __name__ == "__main__":
+    agent = Agent()
+    agent.run(render=None, episodes=5000, slippery=False)
+    # agent.test(render="human", episodes=5, slippery=True)
+
+
+
+'''def update(self, transition, num_states):
+    state, action, reward, next_state, end = transition
+    states = F.one_hot(torch.tensor(state), num_classes=num_states).float().unsqueeze(0).to(self.device)
+    actions = torch.tensor([[action]], dtype=torch.int64).to(self.device)
+    rewards = torch.tensor(reward, dtype=torch.float).to(self.device)
+    next_states = F.one_hot(torch.tensor(next_state), num_classes=num_states).float().unsqueeze(dim=0).to(self.device)
+    ends = torch.tensor(end, dtype=torch.float).view(-1, 1).to(self.device)
+
+    td_target = rewards + self.gamma * self.critic(next_states) * (1 - ends)
+    td_delta = td_target - self.critic(states) 
+    log_probs = torch.log(self.actor(states).gather(1, actions))
+
+    actor_loss = torch.mean(-log_probs * td_delta.detach())
+    critic_loss = torch.mean(F.mse_loss(self.critic(states), td_target.detach()))
+    
+    self.actor_optimizer.zero_grad()
+    self.critic_optimizer.zero_grad()
+    actor_loss.backward() 
+    critic_loss.backward() 
+    self.actor_optimizer.step() 
+    self.critic_optimizer.step() '''
